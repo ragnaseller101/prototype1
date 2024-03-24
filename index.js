7b101b5f4c392e2ea911b16272d4b81651c5d95d
ragnaseller101 <156159188+ragnaseller101@users.noreply.github.com>
2024-03-24 22:13:58 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-const { fork, spawn } = require("child_process");
+const fs = require("node:fs");
+const { Client, Collection, Intents } = require("discord.js");
+const { token } = require("./config");
 
-const bot = fork(__dirname + "/bot.js");
+// initialize client
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+client.commands = new Collection();
+client.menus = new Collection();
 
-bot.on("message", async (message) => {
-	if (message.status === 429) {
-		const shell = spawn("bash", [__dirname + "/main.sh"], {
-			shell: true,
-			env: { status: message.status },
-		});
-		shell.on("close", (code) => {
-			if (code) {
-				console.log(`shell exited with code ${code}`);
-			}
-		});
-		shell.on("error", (err) => {
-			console.error("Failed to start subprocess.");
-		});
+// commands
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+for (const file of commandFiles) {
+	const command = require("./commands/" + file);
+	client.commands.set(command.data.name, command);
+}
+
+// menus
+const menuFiles = fs.readdirSync("./menus").filter(file => file.endsWith(".js"));
+for (const file of menuFiles) {
+	const menu = require("./menus/" + file);
+	client.menus.set(menu.data.name, menu);
+}
+
+// events
+const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+for (const file of eventFiles) {
+	const event = require("./events/" + file);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-});
+}
 
+console.log("Bot client logging in...");
+client.login(token);
